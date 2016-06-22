6c388bb463321aadecf3c9eca528218108d66074
James Ruchala <jruchala@gmail.com>
2016-06-21 23:13:06 -0400

    created javascript controls

--- js/app.js
+++ js/app.js
@@ -6,6 +6,19 @@ $(document).ready(function() {
     .mouseleave(function() {
       $('.ryu-ready').hide();
       $('.ryu-still').show();
+    })
+    .mousedown(function() {
+      
+      // play hadouken sound
+      $('.ryu-ready').hide();
+      $('.ryu-throwing').show();
+      $('.hadouken').show();
+      // show hadouken and animate ti to right of the screen
+    })
+    .mouseup(function() {
+      $('.ryu-throwing').hide();
+      $('.ryu-ready').show();
+      // ryu goes back to his ready position
     });
 
-});
+}); // end
