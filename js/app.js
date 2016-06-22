48e1ab13cdf4ad96cb8a879978720db0122c1d10
James Ruchala <jruchala@gmail.com>
2016-06-21 23:24:51 -0400

    added hadouken sound

--- js/app.js
+++ js/app.js
@@ -9,16 +9,26 @@ $(document).ready(function() {
     })
     .mousedown(function() {
       
-      // play hadouken sound
+      playHadouken();
       $('.ryu-ready').hide();
       $('.ryu-throwing').show();
-      $('.hadouken').show();
-      // show hadouken and animate ti to right of the screen
+      $('.hadouken').finish().show().animate(
+        {'left': '1020px'},
+        500,
+        function() {
+          $(this).hide();
+          $(this).css('left', '520px');
+        });
     })
     .mouseup(function() {
       $('.ryu-throwing').hide();
       $('.ryu-ready').show();
-      // ryu goes back to his ready position
     });
 
-}); // end
+}); // end document ready
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
