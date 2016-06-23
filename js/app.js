a4f4e4d31bb72ee4c7e3401ab1d86e0b79815c8e
James Ruchala <jruchala@gmail.com>
2016-06-22 21:33:43 -0400

    added cool pose on keydown

--- js/app.js
+++ js/app.js
@@ -25,6 +25,22 @@ $(document).ready(function() {
       $('.ryu-ready').show();
     });
 
+    $(document).keydown(function(e) {
+      if (e.keyCode == 88) {
+      
+      $('.ryu-still').hide();
+      $('.ryu-cool').show();
+      
+      }
+    }).keyup(function(e) {
+      if (e.keyCode == 88){
+        
+
+        $('.ryu-cool').hide();
+        $('.ryu-still').show();
+      }
+    })
+
 }); // end document ready
 
 function playHadouken () {
